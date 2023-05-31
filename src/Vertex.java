b771dc8ef453b4e5b72a65e167fc3a5b4f010639
Ернур Нурланов <nurlanovernur33@gmail.com>
2023-05-31 22:20:11 +0600

    Implementation of Vertex class

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,6 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
-public class Vertex<V> {
+public class Vertex<V>{
     private V data;
     private Map<Vertex<V>, Double> adjVertices;
     public void addAdjVertex(Vertex<V> dest, double weight) {
